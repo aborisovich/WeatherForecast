3357158b0ec571de88c086b8a42a86b506d9d62e
Andrey Borisovich <borisovich.andrey@gmail.com>
2020-06-02 16:05:09 +0200

    Refactored OpenWeatherMapController. Removed debug messaging, added documentation.

--- WeatherForecast/Controllers/OpenWeatherMapController.cs
+++ WeatherForecast/Controllers/OpenWeatherMapController.cs
@@ -12,23 +12,31 @@ using Microsoft.Extensions.Logging;
 namespace WeatherForecast.Controllers
 {
     /// <summary>
-    /// Redirects calls from local API to external OpenWeatherMap API.
+    /// Creates local REST API providing weather information using external OpenWeatherMap API.
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class OpenWeatherMapController : ControllerBase
     {
+        /// <summary>
+        /// Contains OpenWeatherAPI prefixes and suffixes used to build http calls.
+        /// </summary>
         private static class ApiCommands
         {
             public static readonly string locationQueryPrefix = "?q=";
             public static readonly string apiKeyPrefix = "&appid=";
-            public static readonly string metricUnitsPostfix = "&units=metric";
+            public static readonly string metricUnitsPrefix = "&units=";
         }
 
         private HttpClient apiClient { get; set; }
         private string ApiKey { get; set; }
-        private readonly string apiUrl;
+        private const string apiUrl = "http://api.openweathermap.org/data/2.5/weather";
 
+        /// <summary>
+        /// Creates object instance containing OpenWeatherMap API key.
+        /// </summary>
+        /// <param name="configuration">Injected application configuration.</param>
+        /// <param name="clientFactory">Injected http client used to create calls to external API.</param>
         public OpenWeatherMapController(IConfiguration configuration, IHttpClientFactory clientFactory)
         {
             apiClient = clientFactory.CreateClient();
@@ -47,20 +55,27 @@ namespace WeatherForecast.Controllers
             if (!apiKey.Exists())
                 throw new ApplicationException("Application configuration entry 'OpenWeatherMapApiKey' is missing a value. Please provide API key");
             ApiKey = apiKey.Value;
-            apiUrl = $"http://api.openweathermap.org/data/2.5/weather";
         }
 
+        /// <summary>
+        /// Performs call to OpenWeatherMap API querying current weather in the requested <paramref name="city"/>.
+        /// </summary>
+        /// <param name="city">City name to check current weather.</param>
+        /// <returns><see cref="System.Net.HttpStatusCode.OK"/> with API response content or redirected status code from API call.</returns>
         [HttpGet("{city}")]
         public async Task<ActionResult<string>> GetWeather(string city)
         {
             if (string.IsNullOrWhiteSpace(city))
                 return BadRequest("City name argument is missing");
-            string apiCall = $"{apiUrl}{ApiCommands.locationQueryPrefix}{city}{ApiCommands.apiKeyPrefix}{ApiKey}{ApiCommands.metricUnitsPostfix}";
-            HttpResponseMessage response = await apiClient.GetAsync(apiCall);
-            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                return Ok(response.Content);
-            return StatusCode((int)response.StatusCode);
+            string apiCall = $"{apiUrl}{ApiCommands.locationQueryPrefix}{city}{ApiCommands.apiKeyPrefix}{ApiKey}{ApiCommands.metricUnitsPrefix}metric";
+            HttpResponseMessage responseMessage = await apiClient.GetAsync(apiCall);
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string responseContent = responseMessage.Content.ReadAsStringAsync().Result;
+                return Ok(responseContent);
+            }
+
+            return StatusCode((int)responseMessage.StatusCode);
         }
     }
 }
